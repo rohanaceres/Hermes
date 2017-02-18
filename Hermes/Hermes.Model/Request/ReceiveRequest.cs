613bd5a0434368c193e64754a4caa55a5d6ac0d3
rohanaceres <ccarvalho@stone.com.br>
2017-02-18 17:53:52 -0200

    :rocket: Add commands to receive messages.

---
+++ Hermes/Hermes.Model/Request/ReceiveRequest.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json;
+
+namespace Hermes.Model.Request
+{
+    // TODO: Doc
+    public sealed class ReceiveRequest
+    {
+        [JsonProperty(PropertyName = "cmd")]
+        public string CommandName { get; set; }
+        [JsonProperty(PropertyName = "id")]
+        public int UserId { get; set; }
+        [JsonProperty(PropertyName = "msgNr")]
+        public int MessageIndex { get; set; }
+    }
+}
---
+++ Hermes/Hermes.Model/Response/ReceiveResponse.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json;
+
+namespace Hermes.Model.Response
+{
+    // TODO: Doc.
+    public sealed class ReceiveResponse
+    {
+        [JsonProperty(PropertyName = "id")]
+        public int UserId { get; set; }
+        [JsonProperty(PropertyName = "msgNr")]
+        public int MessageIndex { get; set; }
+        [JsonProperty(PropertyName = "data")]
+        public string Data { get; set; }
+    }
+}
