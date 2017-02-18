02df0a949b328b7fda4531c4c800afc50bbe1317
rohanaceres <ccarvalho@stone.com.br>
2017-02-18 17:33:40 -0200

    :rocket: Add request for sending messages.

---
+++ Hermes/Hermes.Model/Request/SendRequest.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+
+namespace Hermes.Model.Request
+{
+    // TODO: Doc.
+    public sealed class SendRequest
+    {
+        [JsonProperty(PropertyName = "cmd")]
+        public string CommandName { get; set; }
+        [JsonProperty(PropertyName = "id")]
+        public int UserId { get; set; }
+        [JsonProperty(PropertyName = "msgNr")]
+        public int MessageIndex { get; set; }
+        [JsonProperty(PropertyName = "dst")]
+        public int DestinationUserId { get; set; }
+        [JsonProperty(PropertyName = "data")]
+        public string Data { get; set; }
+    }
+}
---
+++ Hermes/Hermes.Model/Response/SendResponse.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+
+namespace Hermes.Model.Response
+{
+    // TODO: Doc.
+    public sealed class SendResponse
+    {
+        [JsonProperty(PropertyName = "id")]
+        public int UserId { get; set; }
+        [JsonProperty(PropertyName = "msgNr")]
+        public int MessageIndex { get; set; }
+    }
+}
